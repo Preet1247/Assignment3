b9e06653db90595909bd5e8ed485d346852118e7
Preet1247 <92147787+Preet1247@users.noreply.github.com>
2024-11-08 19:12:30 +1300

    Create credentials.php

---
+++ credentials.php
@@ -0,0 +1,6 @@
+<?php 
+// Database credentials
+$user = "a30049667";  // Your database username
+$pw = "Toiohomai1234";    // Your database password
+$db = "a30049667_db";     // Your database name
+?>
